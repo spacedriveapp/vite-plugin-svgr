6d641fefc68215b03bd83e5c770864aa225adbb7
Brendan Allan <brendonovich@outlook.com>
2022-05-01 10:53:38 +0800

    stella's fix

--- src/index.ts
+++ src/index.ts
@@ -16,30 +16,50 @@ export = function svgrPlugin({
     name: 'vite:svgr',
     async transform(code, id) {
       if (id.endsWith('.svg')) {
-        const { transform: convert } = await import('@svgr/core')
 
-        const svgCode = await fs.promises.readFile(id, 'utf8')
+        const { transform: convert } = await import('@svgr/core');
 
-        const componentCode = await convert(svgCode, svgrOptions, {
-          componentName: 'ReactComponent',
-          filePath: id,
-        }).then((res) => {
-          return res.replace(
-            'export default ReactComponent',
-            `export { ReactComponent }`
-          )
-        })
+        const svgCode = await fs.promises.readFile(id, 'utf8');
 
-        const res = await transformWithEsbuild(
-          componentCode + '\n' + code,
-          id,
-          { loader: 'jsx', ...esbuildOptions }
-        )
+        const componentCode = await convert(
+          svgCode,
+          {
+            ...svgrOptions,
+            plugins: [...(svgrOptions?.plugins ?? []), '@svgr/plugin-svgo', '@svgr/plugin-jsx'],
+            svgoConfig: {
+              ...(svgrOptions?.svgoConfig ?? {}),
+              plugins: [
+                {
+                  name: 'preset-default',
+                  params: {
+                    overrides: {
+                      inlineStyles: {
+                        onlyMatchedOnce: false
+                      }
+                    }
+                  }
+                },
+                'removeStyleElement'
+              ]
+            }
+          },
+          {
+            componentName: 'ReactComponent',
+            filePath: id
+          }
+        ).then((res) => {
+          return res.replace('export default ReactComponent', `export { ReactComponent }`);
+        });
+
+        const res = await transformWithEsbuild(componentCode + '\n' + code, id, {
+          loader: 'jsx',
+          ...esbuildOptions
+        });
 
         return {
           code: res.code,
-          map: null, // TODO:
-        }
+          map: null
+        };
       }
     },
   }
